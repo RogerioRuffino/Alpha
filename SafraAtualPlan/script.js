e34f912330f196149a39abd59bc9c594c6f1ef64
ROGERIO RUFINO <92214783+RogerioRuffino@users.noreply.github.com>
2023-04-22 11:25:08 -0300

    Update script.js

--- SafraAtualPlan/script.js
+++ SafraAtualPlan/script.js
@@ -15,7 +15,6 @@ callCadastrarProdutor.addEventListener('click', () => {
 // selecionando os elementos HTML
 const clientesSelect = document.querySelector('#clientes');
 const safraSelect = document.querySelector('#safra');
-// const btnAdicionarCliente = document.querySelector('#btn_adicionar_cliente');
 const contentDiv = document.querySelector('.content');
 
 clientesSelect.addEventListener('change', () => {
@@ -25,40 +24,42 @@ clientesSelect.addEventListener('change', () => {
 safraSelect.addEventListener('change', () => {
     contentDiv.style.display = 'block';
 });
-// =================================================
-// Saltar campo quando presionado 2 vezes enter no input e selecionado option com click
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-const inputs = document.querySelectorAll('.sel2');
-let lastKeyPressTime = 0;
+  
 
-inputs.forEach((input, index) => {
-  input.addEventListener('keydown', event => {
-    if (event.key === 'Tab') {
-      const now = new Date().getTime();
-      if (now - lastKeyPressTime < 200) {
-        event.preventDefault();
-        if (index + 1 < inputs.length) {
-          inputs[index + 1].focus();
-        } else {
-          inputs[0].focus();
+// =================================
+//  Saltar campo quando presionado 2 vezes enter no input e selecionado option com click
+
+  const inputs = document.querySelectorAll('.sel1, .sel2');
+  let lastKeyPressTime = 0;
+
+  inputs.forEach((input, index) => {
+    input.addEventListener('keydown', event => {
+      if (event.key === 'Tab') {
+        const now = new Date().getTime();
+        if (now - lastKeyPressTime < 200) {
+          event.preventDefault();
+          if (index + 1 < inputs.length) {
+            inputs[index + 1].focus();
+          } else {
+            inputs[0].focus();
+          }
         }
+        lastKeyPressTime = now;
       }
-      lastKeyPressTime = now;
-    }
-  });
+    });
 
-  input.addEventListener('change', event => {
-    if (index + 1 < inputs.length) {
-      inputs[index + 1].focus();
-    } else {
-      inputs[0].focus();
-    }
+    input.addEventListener('change', event => {
+      if (index + 1 < inputs.length) {
+        inputs[index + 1].focus();
+      } else {
+        inputs[0].focus();
+      }
+    });
   });
-});
 
 // ==================================================
-
-
 
 
 function carregarProdutos(idSelect, nomeArquivo) {
@@ -132,118 +133,116 @@ class Produto {
 
          
         }
-        //  console.log(produto);
-        // console.log(this.arrayProdutos);
-        
+
         this.listaTabela();
         this.limpar();
     } 
 
-        listaTabela(){
-            let tbody = document.getElementById('tbody');
-            tbody.innerText = '';
-
-            let valorTotalGeral = 0;
-            
-            
-
-            for(let i = 0; i < this.arrayProdutos.length; i++ ) {
-                let tr = tbody.insertRow();
-
-                let td_id = tr.insertCell();
-                let td_produto = tr.insertCell();
-                let td_embalagem = tr.insertCell();
-                let td_segmento = tr.insertCell();
-                let td_volume = tr.insertCell();
-                let td_fornecedor = tr.insertCell();
-                let td_valorunitario = tr.insertCell();
-                let td_unidade = tr.insertCell();
-                let td_valortotal = tr.insertCell();
-                let td_vencimento = tr.insertCell();
-                let td_acoes = tr.insertCell();
-                // console.log (td_id,td_commoditie,td_precosc)
-
-
-                td_id.innerText = this.arrayProdutos[i].id;     
-                td_produto.innerText = this.arrayProdutos[i].produto;
-                td_embalagem.innerText = this.arrayProdutos[i].embalagem;
-                td_segmento.innerText = this.arrayProdutos[i].segmento;
-                td_volume.innerText = this.arrayProdutos[i].volume.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
-                td_fornecedor.innerText = this.arrayProdutos[i].fornecedor;
-                td_valorunitario.innerText = this.arrayProdutos[i].valorunitario.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-                td_unidade.innerText = this.arrayProdutos[i].unidade;
-              
-                td_valortotal.innerText = this.arrayProdutos[i].valortotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-
-        valorTotalGeral += this.arrayProdutos[i].valortotal;
-
-                const data = new Date(this.arrayProdutos[i].vencimento);
-                const dia = String(data.getDate()).padStart(2, '0');
-                const mes = String(data.getMonth() + 1).padStart(2, '0');
-                const ano = data.getFullYear();
-                const dataFormatada = `${dia}-${mes}-${ano}`;
-                td_vencimento.innerText = dataFormatada;    
-        
-                let iconEdit = document.createElement('i');
-                iconEdit.classList.add('fas', 'fa-pen-to-square', 'icon');
-                iconEdit.setAttribute("onclick", "produto.preparaEdicao(" + JSON.stringify(this.arrayProdutos[i]) + ")");
-
-                let iconDelete = document.createElement('i');
-                iconDelete.classList.add('fas', 'fa-trash', 'icon');
-                iconDelete.setAttribute("onclick", "produto.deletar("+ this.arrayProdutos[i].id +")");
-
-                td_acoes.appendChild(iconEdit);
-                td_acoes.appendChild(iconDelete);
-              
-            }
-            // criando uma nova linha para o valor total geral
-    let tr = tbody.insertRow();
     
+    listaTabela(){
+        let tbody = document.getElementById('tbody');
+        tbody.innerText = '';
 
-      // criando a célula com o texto "valorTotalGeral"
-    let td_total = tr.insertCell();
-    td_total.innerText = "Valor Total Geral";
+        let valorTotalGeral = 0;
+        
+        
 
-    // criando as células vazias antes do valor total geral
-    let td_empty1 = tr.insertCell();
-    td_empty1.colSpan = 7;
+        for(let i = 0; i < this.arrayProdutos.length; i++ ) {
+            let tr = tbody.insertRow();
 
-    // criando a célula com o valor total geral
-    let td_total_value = tr.insertCell();
-    td_total_value.innerText = valorTotalGeral.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-
-    // criando as células vazias depois do valor total geral
-    let td_empty2 = tr.insertCell();
-    td_empty2.colSpan = 1;
-    // <i class="fa-regular fa-download"></i>
-    let td_iconUpload = tr.insertCell();
-    td_iconUpload.innerHTML = '<i class="fa-solid fa-download"></i>';
-    // adicionando classe CSS à última linha
-    tr.classList.add('total-row');
-
-    td_iconUpload.classList.add('icon2');
-    td_iconUpload.onclick = function() {
-        salvarProdutorSafra();
-    }    
-        };
+            let td_id = tr.insertCell();
+            let td_produto = tr.insertCell();
+            let td_embalagem = tr.insertCell();
+            let td_segmento = tr.insertCell();
+            let td_volume = tr.insertCell();
+            let td_fornecedor = tr.insertCell();
+            let td_valorunitario = tr.insertCell();
+            let td_unidade = tr.insertCell();
+            let td_valortotal = tr.insertCell();
+            let td_vencimento = tr.insertCell();
+            let td_acoes = tr.insertCell();
 
 
+            td_id.innerText = this.arrayProdutos[i].id;     
+            td_produto.innerText = this.arrayProdutos[i].produto;
+            td_embalagem.innerText = this.arrayProdutos[i].embalagem;
+            td_segmento.innerText = this.arrayProdutos[i].segmento;
+            td_volume.innerText = this.arrayProdutos[i].volume.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+            td_fornecedor.innerText = this.arrayProdutos[i].fornecedor;
+            td_valorunitario.innerText = this.arrayProdutos[i].valorunitario.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+            td_unidade.innerText = this.arrayProdutos[i].unidade;
+          
+            td_valortotal.innerText = this.arrayProdutos[i].valortotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+            valorTotalGeral += this.arrayProdutos[i].valortotal;
+
+            const data = new Date(this.arrayProdutos[i].vencimento);
+            const dia = String(data.getDate()).padStart(2, '0');
+            const mes = String(data.getMonth() + 1).padStart(2, '0');
+            const ano = data.getFullYear();
+            const dataFormatada = `${dia}-${mes}-${ano}`;
+            td_vencimento.innerText = dataFormatada;    
+    
+            let iconEdit = document.createElement('i');
+            iconEdit.classList.add('fas', 'fa-pen-to-square', 'icon');
+            iconEdit.setAttribute("onclick", "produto.preparaEdicao(" + JSON.stringify(this.arrayProdutos[i]) + ")");
+
+            let iconDelete = document.createElement('i');
+            iconDelete.classList.add('fas', 'fa-trash', 'icon');
+            iconDelete.setAttribute("onclick", "produto.deletar("+ this.arrayProdutos[i].id +")");
+
+            td_acoes.appendChild(iconEdit);
+            td_acoes.appendChild(iconDelete);
+          
+        }
+        // criando uma nova linha para o valor total geral
+        let tr = tbody.insertRow();
+
+
+        // criando a célula com o texto "valorTotalGeral"
+        let td_total = tr.insertCell();
+        td_total.innerText = "Valor Total Geral";
+
+        // criando as células vazias antes do valor total geral
+        let td_empty1 = tr.insertCell();
+        td_empty1.colSpan = 7;
+
+        // criando a célula com o valor total geral
+        let td_total_value = tr.insertCell();
+        td_total_value.innerText = valorTotalGeral.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+        // criando as células vazias depois do valor total geral
+        let td_empty2 = tr.insertCell();
+        td_empty2.colSpan = 1;
+        // <i class="fa-regular fa-download"></i>
+        let td_iconUpload = tr.insertCell();
+        td_iconUpload.innerHTML = '<i class="fa-solid fa-download"></i>';
+        // adicionando classe CSS à última linha
+        tr.classList.add('total-row');
+
+        td_iconUpload.classList.add('icon2');
+        td_iconUpload.onclick = function() {
+            salvarProdutorSafra();
+        }    
+    };
+
+    
     adicionar(produto) {
         
-        // produto.produto = parseFloat(produto.produto);
-        // produto.embalagem = parseFloat(produto.embalagem);
-        // produto.segmento = parseFloat(produto.segmento);
+        produto.produto = produto.produto;
+        produto.embalagem = produto.embalagem;
+        produto.segmento = produto.segmento;
         produto.volume = parseFloat(produto.volume);
-        // produto.fornecedor = parseFloat(produto.fornecedor);
+        produto.fornecedor = produto.fornecedor;
         produto.valorunitario = parseFloat(produto.valorunitario);
         produto.valortotal = parseFloat(produto.volume * produto.valorunitario);
+        produto.vencimento = produto.vencimento;
 
-        // produto.vencimento = parseFloat(produto.vencimento);
         this.arrayProdutos.push(produto);
         this.id++;
 
-  
-    }
+}
+
 
     atualizar(id, produto) {
 
@@ -252,19 +251,15 @@ class Produto {
     
                 this.arrayProdutos[i].produto = produto.produto;
                 this.arrayProdutos[i].embalagem = produto.embalagem;
-                this.arrayProdutos[i].segmento = produto.segmento;
                 this.arrayProdutos[i].volume = produto.volume;
                 this.arrayProdutos[i].fornecedor = produto.fornecedor;
                 this.arrayProdutos[i].valorunitario = produto.valorunitario;
                 this.arrayProdutos[i].unidade = produto.unidade;
-                // this.arrayProdutos[i].valortotal = produto.valortotal;
+                this.arrayProdutos[i].valortotal =parseFloat(produto.volume * produto.valorunitario);
                 this.arrayProdutos[i].vencimento = produto.vencimento;
 
             }
         }
-
-           
-
     }
     
     lerDados() {
@@ -272,8 +267,7 @@ class Produto {
         let produto = {};
 
         produto.id = this.id;
-        produto.prod = document.getElementById('produto').value;
-        // produto.produto = retornaOption('produto');
+        produto.produto = document.getElementById('produto').value;
         produto.embalagem = document.getElementById('embalagem').value;
         produto.segmento = document.getElementById('segmento').value;
         produto.volume = document.getElementById('volume').value;
@@ -319,9 +313,7 @@ class Produto {
         if(produto.vencimento == ""){
             msg += ' - selecione a data do vencimento da compra \n'
         }
-        // if(produto.precokg == ""){
-        //     msg += 'informe o preço por kg commoditie \n'
-        // }
+
         if (msg != '') {
             alert(msg);
             return false
@@ -337,7 +329,7 @@ class Produto {
         document.getElementById('fornecedor').value = 'Fornecedor';
         document.getElementById('valorunitario').value = '';
         document.getElementById('unidade').value = 'Unidade';
-        // document.getElementById('valortotal').value = '';
+
         document.getElementById('vencimento').value = '';
        
 
@@ -348,7 +340,7 @@ class Produto {
         
     }
 
-    
+
     deletar(id) {
         
         let tbody = document.getElementById('tbody');
@@ -358,24 +350,18 @@ class Produto {
                 this.arrayProdutos.splice(i, 1);
                 tbody.deleteRow(i);
             }
+        this.listaTabela();   
         }
     }
     
     limparTudo() {
         let tbody = document.getElementById('tbody');
-        document.querySelector('#clientes').value='';
-        document.querySelector('#safra').value='';
         while (tbody.rows.length > 0) {
             tbody.deleteRow(0);
         }
     
-        this.arrayProdutos = [];
     }
 
-    
-    
-    
-    
 
     preparaEdicao(dados) {
         
@@ -387,7 +373,6 @@ class Produto {
         document.getElementById('fornecedor').value = dados.fornecedor;
         document.getElementById('valorunitario').value = dados.valorunitario;
         document.getElementById('unidade').value = dados.unidade;
-        // document.getElementById('valortotal').value = dados.valortotal;
         document.getElementById('vencimento').value = dados.vencimento;
 
         document.getElementById('btn1').innerText = 'Atualizar';
